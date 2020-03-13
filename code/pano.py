aec5fd1b8aeed04190747ea0fd2f03f7a54df647
Lukas Weber <lukas.weber@baw.de>
2020-03-13 13:39:20 +0100

    Python 3, OpenCV 3.3

--- code/pano.py
+++ code/pano.py
@@ -1,3 +1,6 @@
+# usage:
+# python pano.py txtlists/filename_.txt
+
 import numpy as np
 import cv2
 import sys
@@ -9,7 +12,7 @@ class Stitch:
 		self.path = args
 		fp = open(self.path, 'r')
 		filenames = [each.rstrip('\r\n') for each in  fp.readlines()]
-		print filenames
+		print(filenames)
 		self.images = [cv2.resize(cv2.imread(each),(480, 320)) for each in filenames]
 		self.count = len(self.images)
 		self.left_list, self.right_list, self.center_im = [], [],None
@@ -17,28 +20,28 @@ class Stitch:
 		self.prepare_lists()
 
 	def prepare_lists(self):
-		print "Number of images : %d"%self.count
+		print("Number of images : %d"%self.count)
 		self.centerIdx = self.count/2 
-		print "Center index image : %d"%self.centerIdx
+		print("Center index image : %d"%self.centerIdx)
 		self.center_im = self.images[int(self.centerIdx)]
 		for i in range(self.count):
 			if(i<=self.centerIdx):
 				self.left_list.append(self.images[i])
 			else:
 				self.right_list.append(self.images[i])
-		print "Image lists prepared"
+		print("Image lists prepared")
 
 	def leftshift(self):
 		# self.left_list = reversed(self.left_list)
 		a = self.left_list[0]
 		for b in self.left_list[1:]:
 			H = self.matcher_obj.match(a, b, 'left')
-			print "Homography is : ", H
+			print("Homography is : ", H)
 			xh = np.linalg.inv(H)
-			print "Inverse Homography :", xh
+			print ("Inverse Homography :", xh)
 			ds = np.dot(xh, np.array([a.shape[1], a.shape[0], 1]));
 			ds = ds/ds[-1]
-			print "final ds=>", ds
+			print ("final ds=>", ds)
 			f1 = np.dot(xh, np.array([0,0,1]))
 			f1 = f1/f1[-1]
 			xh[0][-1] += abs(f1[0])
@@ -47,7 +50,7 @@ class Stitch:
 			offsety = abs(int(f1[1]))
 			offsetx = abs(int(f1[0]))
 			dsize = (int(ds[0])+offsetx, int(ds[1]) + offsety)
-			print "image dsize =>", dsize
+			print ("image dsize =>", dsize)
 			tmp = cv2.warpPerspective(a, xh, dsize)
 			# cv2.imshow("warped", tmp)
 			# cv2.waitKey()
@@ -60,17 +63,17 @@ class Stitch:
 	def rightshift(self):
 		for each in self.right_list:
 			H = self.matcher_obj.match(self.leftImage, each, 'right')
-			print "Homography :", H
+			print ("Homography :", H)
 			txyz = np.dot(H, np.array([each.shape[1], each.shape[0], 1]))
 			txyz = txyz/txyz[-1]
 			dsize = (int(txyz[0])+self.leftImage.shape[1], int(txyz[1])+self.leftImage.shape[0])
 			tmp = cv2.warpPerspective(each, H, dsize)
-			cv2.imshow("tp", tmp)
-			cv2.waitKey()
+			#cv2.imshow("tp", tmp)
+			#cv2.waitKey()
 			# tmp[:self.leftImage.shape[0], :self.leftImage.shape[1]]=self.leftImage
 			tmp = self.mix_and_match(self.leftImage, tmp)
-			print "tmp shape",tmp.shape
-			print "self.leftimage shape=", self.leftImage.shape
+			print ("tmp shape",tmp.shape)
+			print ("self.leftimage shape=", self.leftImage.shape)
 			self.leftImage = tmp
 		# self.showImage('left')
 
@@ -79,13 +82,13 @@ class Stitch:
 	def mix_and_match(self, leftImage, warpedImage):
 		i1y, i1x = leftImage.shape[:2]
 		i2y, i2x = warpedImage.shape[:2]
-		print leftImage[-1,-1]
+		print (leftImage[-1,-1])
 
 		t = time.time()
 		black_l = np.where(leftImage == np.array([0,0,0]))
 		black_wi = np.where(warpedImage == np.array([0,0,0]))
-		print time.time() - t
-		print black_l[-1]
+		print (time.time() - t)
+		print (black_l[-1])
 
 		for i in range(0, i1x):
 			for j in range(0, i1y):
@@ -134,13 +137,13 @@ if __name__ == '__main__':
 	except:
 		args = "txtlists/files1.txt"
 	finally:
-		print "Parameters : ", args
+		print ("Parameters : ", args)
 	s = Stitch(args)
 	s.leftshift()
 	# s.showImage('left')
 	s.rightshift()
-	print "done"
+	print ("done")
 	cv2.imwrite("test12.jpg", s.leftImage)
-	print "image written"
+	print ("image written")
 	cv2.destroyAllWindows()
 	
